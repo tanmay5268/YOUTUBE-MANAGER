d5843f5412fdb1b9d38dcfc8ff5b3d3db5903846
TANMAY MEWATI <tanmay.mewati94@gmail.com>
2025-04-05 18:49:53 +0530

    update videos added but some bugs are there trying to figure out

--- yt_manager_mongo.py
+++ yt_manager_mongo.py
@@ -1,6 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 import pymongo.errors
+from bson.objectid import ObjectId
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 database = client['yt_manager']
 vid_collection = database['videos']
@@ -29,6 +30,19 @@ def addvideo(name,time):
     }
     result=vid_collection.insert_one(video_data)
     print(f"Videos added with id:{result.inserted_id}")
+def updatevideos(index,newname,newtime):
+    try:
+        object_id=ObjectId(index)
+        result = vid_collection.update_one(
+            {"_id":object_id},{"$set":{'name':newname,'time':newtime}}
+              )
+        if vid_collection.modified_count > 0:
+            print(f"video id {index} updated succesfully")
+        else:
+            print(f"video id{index} not found!!!!")
+        listvideos()
+    except Exception as f:
+        print(f"some error occured: {f}")
 
 def main():
     print('\n')
@@ -48,14 +62,14 @@ def main():
                 listvideos()
             case 2 :
                 name =input('ENTER NAME OF NEW VIDEO:')
-                time =int(input('ENTER DURATION:'))
+                time =input('ENTER DURATION:')
                 addvideo(name,time)
             case 3:
                 listvideos()
-                index= int(input('ENTER VIDEO INDEX U WANNA CHANGE BLUD:'))
+                index= input('ENTER VIDEO INDEX U WANNA CHANGE BLUD:')
                 newname = input('ENTER NEW NAME:')
-                newtime = int(input('CHANGE TIME:'))
-                updatevideos()
+                newtime = input('CHANGE TIME:')
+                updatevideos(index,newname,newtime)
             case 4:
                 listvideos()
                 index= int(input('ENTER VIDEO INDEX U WANNA DELETE BLUD:')) 
