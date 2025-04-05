a294775c36d99039ae01fac0de2eb1ded2577e91
TANMAY MEWATI <tanmay.mewati94@gmail.com>
2025-04-05 14:13:46 +0530

    thodi sundarta added

--- yt_manager_mongo.py
+++ yt_manager_mongo.py
@@ -6,20 +6,26 @@ database = client['yt_manager']
 vid_collection = database['videos']
 try:
     client.server_info()
+    print('*'*50)
     print('connection succeseful')
+    print('*'*50)
 except pymongo.errors.ServerSelectionTimeoutError:
     print('somethimg went really wrong bro!!!!!!')
     exit()
 
 def main():
+    print('\n')
     print('WELCOME TO MONGO--YT MANAGER')
+    print('*'*50)
     while True:
         print('1.list all videos')
         print('2.add your videos')
         print('3.update existing videos')
         print('4.delete videos')
         print('5.exit from heaven :)')
+        print('*'*50)
         choice=int(input('enter your choice:-'))
+        print('*'*50)
         match choice:
             case 1 :
                 listvideos()
